5cec517315cd3e4e7f04216188cce262cef313e1
YOUSSEF EL BOUNDATI <elboundati1234@gmail.com>
2025-12-14 20:42:12 +0100

    ajouter models

---
+++ src/app/models/produit-facture.spec.ts
@@ -0,0 +1,7 @@
+import { ProduitFacture } from './produit-facture';
+
+describe('ProduitFacture', () => {
+  it('should create an instance', () => {
+    expect(new ProduitFacture()).toBeTruthy();
+  });
+});
